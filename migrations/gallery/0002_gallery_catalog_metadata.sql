22f610fcc1d48a6e41c3c7bad5769e1a7ef10866
Christian Obe <110026838+chris-obe@users.noreply.github.com>
2026-06-29 01:55:42 +0100

    Add Cloudflare gallery storage backing

---
+++ migrations/gallery/0002_gallery_catalog_metadata.sql
@@ -0,0 +1,3 @@
+ALTER TABLE gallery_photos ADD COLUMN camera_catalog_id TEXT;
+ALTER TABLE gallery_photos ADD COLUMN lens_catalog_id TEXT;
+ALTER TABLE gallery_photos ADD COLUMN metadata_source_json TEXT;
